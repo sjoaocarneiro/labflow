db265df7684d1cae6621ce26a23fd903d956304f
sjoaocarneiro <sjoaocarneiro@gmail.com>
2026-07-03 20:29:04 -0300

    Update power-up.js

--- power-up.js
+++ power-up.js
@@ -1,13 +1,16 @@
 window.TrelloPowerUp.initialize({
-  'card-buttons': function (t, options) {
-    return [{
-      text: 'LabFlow',
-      callback: function (t) {
-        return t.popup({
-          title: 'LabFlow',
-          url: './index.html'
-        });
+  "card-buttons": function (t, options) {
+    return [
+      {
+        text: "LabFlow",
+        callback: function (t) {
+          return t.popup({
+            title: "LabFlow",
+            url: "pages/home.html",
+            height: 250
+          });
+        }
       }
-    }];
+    ];
   }
 });
