6d236816ad2eca2ad1d4dd4a02ac912d5a320dcb
sjoaocarneiro <sjoaocarneiro@gmail.com>
2026-07-03 20:45:12 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,6 +2,6 @@ document
     .getElementById("btnAssinar")
     .addEventListener("click", function () {
 
-        alert("Botão funcionando!");
+        window.location.href = "signature.html";
 
     });
