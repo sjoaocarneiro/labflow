3a38b92bd697ed562c262e24ba4eefd7ae960674
sjoaocarneiro <sjoaocarneiro@gmail.com>
2026-07-03 21:53:10 -0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,1 +1,11 @@
+async function uploadSignature(cardId, dataURL) {
 
+    const blob = await (await fetch(dataURL)).blob();
+
+    const formData = new FormData();
+
+    formData.append("file", blob, "assinatura.png");
+
+    return { formData };
+
+}
