c338b2f57dac4f3744fceb8c62e5ec39d183110d
sjoaocarneiro <sjoaocarneiro@gmail.com>
2026-07-03 21:44:46 -0300

    Update trello.js

--- js/trello.js
+++ js/trello.js
@@ -1,1 +1,16 @@
 const t = window.TrelloPowerUp.iframe();
+
+/**
+ * Retorna as informações do cartão atual
+ */
+async function getCard() {
+    return await t.card("id", "name");
+}
+
+/**
+ * Retorna apenas o ID do cartão
+ */
+async function getCardId() {
+    const card = await getCard();
+    return card.id;
+}
