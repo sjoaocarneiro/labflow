96d28f0b05ca52b0f9994c03c41325f70894ff59
sjoaocarneiro <sjoaocarneiro@gmail.com>
2026-07-03 21:57:37 -0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -6,6 +6,18 @@ async function uploadSignature(cardId, dataURL) {
 
     formData.append("file", blob, "assinatura.png");
 
-    return { formData };
+    const response = await fetch(
+        `https://api.trello.com/1/cards/${cardId}/attachments?key=${CONFIG.API_KEY}&token=${CONFIG.TOKEN}`,
+        {
+            method: "POST",
+            body: formData
+        }
+    );
 
+    if (!response.ok) {
+        const erro = await response.text();
+        throw new Error(erro);
+    }
+
+    return await response.json();
 }
